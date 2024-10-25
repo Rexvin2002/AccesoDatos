2bc0641f23e764c998dd56cae6850a0a9e59e309
Kevin Gómez Valderas <kgv1722002@hotmail.com>
2024-10-25 22:59:04 +0200

    ServicioProcesos

---
+++ src/Unidad03/Practica02/App.java
@@ -0,0 +1,53 @@
+package Unidad03.Practica02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // Información de la base de datos
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "root";
+        String password = "passwd";
+        
+        try {
+            // Carga el DRIVER MYSQL manualmente
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded successfully!");
+
+            try ( // Establecer la conexión
+                Connection conn = DriverManager.getConnection(url, user, password)) {
+                System.out.println("Connection established successfully!");
+                
+                // Crear una sentencia SQL
+                Statement stmt = conn.createStatement();
+                
+                // Ejecutar una consulta SQL
+                String query = "SELECT * FROM users"; 
+                ResultSet rs = stmt.executeQuery(query);
+                
+                // Procesa cada uno de los resultados
+                while (rs.next()) {
+                    
+                    System.out.println("ID: " + rs.getInt("id") + ", Name: " + rs.getString("name"));
+                }
+                
+                // Cierra los recursos
+                rs.close();
+                stmt.close();
+            }
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            
+            System.err.println("Error: " + ex.getMessage());
+            
+        }
+        
+    }
+    
+}
---
+++ src/Unidad03/Pruebas.java
@@ -0,0 +1,17 @@
+
+package Unidad03;
+
+/**
+ *
+ * @author kgv17
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
