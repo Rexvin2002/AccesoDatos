2f45c8e3141ae7bbb196760caf0457ccf59c08f2
Kevin Gómez Valderas <kgv1722002@hotmail.com>
2024-11-10 05:35:39 +0100

    Controlador JPA

---
+++ src/Unidad04/springexample/src/main/Entities/Clientes.java
@@ -0,0 +1,68 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Clientes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEmpleado;
+
+    @Column(name = "nombre", unique = true, length = 40)
+    private String nombre;
+
+    @Column(name = "apellido", length = 40)
+    private String apellido;
+
+    @Column(name = "direccion", length = 100)
+    private String direccion;
+
+    // Constructor vacío
+    public Clientes() {}
+
+    // Constructor con parámetros
+    public Clientes(String nombre, String apellido, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+
+    // Getters y Setters
+    public Long getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Long idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
+
